e4dcb9682cd51c85f7c64c3808fce3d5f0fb1d01
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2016-09-27 17:22:57 -0300

    feat(Elfo): implementar tema do dia 01 (experiencia, flechas 42, refactory).

--- src/modulo-01-java-OO/modulo-01/Elfo.java
+++ src/modulo-01-java-OO/modulo-01/Elfo.java
@@ -2,31 +2,31 @@ public class Elfo {
     private String nome;
     private Item arco;
     private Item flecha;
-    
+    private int experiencia;
+
     public Elfo(String n) {
         nome = n;
         arco = new Item("Arco", 1);
-        flecha = new Item("Flechas", 2);
+        flecha = new Item("Flechas", 42);
     }
-    
+
     public void setNome(String n) {
         nome = n;
     }
-    
+
     public String getNome() {
         return nome;
     }
-    
+
     public void atirarFlecha() {
         flecha.setQuantidade(flecha.getQuantidade() - 1);
+        experiencia++;
     }
+
+    /*public void atirarFlechaRefactory() {
+        experiencia++;
+        flecha.setQuantidade(flecha.getQuantidade()-1);
+    }*/
 }
-
-
-
-
-
-
-
 
 
