4833c18fe619e3578a45a196a88f41e3dbdc4b33
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2016-09-28 14:33:21 -0300

    test(Elfo): implementar cenários para quantidades iniciais e atirarFlecha.

--- src/modulo-01-java-OO/modulo-01/ElfoTest.java
+++ src/modulo-01-java-OO/modulo-01/ElfoTest.java
@@ -23,4 +23,151 @@ public class ElfoTest
         assertEquals("Arco", elfoDoTeste.getArco().getDescricao());
         assertEquals(1, elfoDoTeste.getArco().getQuantidade());
     }
+
+    @Test
+    public void elfoNasceCom42Flechas() {
+        // Act
+        Elfo elfoDoTeste = new Elfo("Elrond");
+        // Assert
+        assertEquals("Flechas", elfoDoTeste.getFlecha().getDescricao());
+        assertEquals(42, elfoDoTeste.getFlecha().getQuantidade());
+    }
+
+    @Test
+    public void elfoAtiraUmaFlecha() {
+        // Arrange
+        Elfo legolas = new Elfo("Elfo");
+        // Act
+        legolas.atirarFlecha();
+        // Assert
+        assertEquals(41, legolas.getFlecha().getQuantidade());
+        assertEquals(1, legolas.getExperiencia());
+    }
+
+    @Test
+    public void elfoAtiraDuasFlecha() {
+        // Arrange
+        Elfo legolas = new Elfo("Elfo");
+        // Act
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        // Assert
+        assertEquals(40, legolas.getFlecha().getQuantidade());
+        assertEquals(2, legolas.getExperiencia());
+    }
+    
+    @Test
+    public void elfoNaoAtiraFlecha() {
+        // Arrange & Act
+        Elfo legolas = new Elfo("Elfo");
+        // Assert
+        assertEquals(42, legolas.getFlecha().getQuantidade());
+        assertEquals(0, legolas.getExperiencia());
+    }
+
+    @Test
+    public void elfoAtira42Flechas() {
+        // Arrange
+        Elfo legolas = new Elfo("Elfo");
+        // Act
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        // Assert
+        assertEquals(0, legolas.getFlecha().getQuantidade());
+        assertEquals(42, legolas.getExperiencia());
+    }
+
+    @Test
+    public void elfoAtira43Flechas() {
+        // Arrange
+        Elfo legolas = new Elfo("Elfo");
+        // Act
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        legolas.atirarFlecha();
+        // Assert
+        assertEquals(-1, legolas.getFlecha().getQuantidade());
+        assertEquals(43, legolas.getExperiencia());
+    }
 }
+
