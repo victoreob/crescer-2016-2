14a2f2452b3a7ce1d104939389b2bad8a18c996e
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2016-10-03 17:18:50 -0300

    refactor(IrishDwarf): remover duplicações extendendo a classe Dwarf.

--- src/modulo-01-java-OO/modulo-01/IrishDwarf.java
+++ src/modulo-01-java-OO/modulo-01/IrishDwarf.java
@@ -1,95 +1,11 @@
-public class IrishDwarf {
-    private int vida, experiencia;
-    private DataTerceiraEra dataNascimento;
-    private String nome;
-    private Status status; //= Status.VIVO;
-    private Inventario inventario;
-
-    // java type initializer
-    // vai ser replicado para cada construtor
-    {
-        vida = 110;
-        //status = Status.VIVO;
-    }
+public class IrishDwarf extends Dwarf {
 
     public IrishDwarf() {
         this(null, new DataTerceiraEra(1,1,1));
     }
 
     public IrishDwarf(String nome, DataTerceiraEra dataNascimento) {
-        this.nome = nome;
-        this.dataNascimento = dataNascimento;
-        this.status = Status.VIVO;
-        this.inventario = new Inventario();
-    }
-
-    public void perderVida() {
-        boolean estaMorto = status.equals(Status.MORTO);
-        // sai do método
-        if (estaMorto) {
-            return;
-        }
-
-        double numero = this.getNumeroSorte();
-        if (numero < 0) {
-            this.experiencia += 2;
-        }
-        if (numero > 100) {
-            int vidaAposFlechada = this.vida - 10;
-            if (vidaAposFlechada == 0) {
-                status = Status.MORTO;
-            }
-            if (vida > 0) {
-                vida = vidaAposFlechada;
-            }
-        }
-    }
-
-    public int getVida() {
-        return vida;
-    }
-
-    public int getExperiencia() {
-        return experiencia;
-    }
-
-    public String getNome() {
-        return this.nome;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-    
-    public Inventario getInventario() {
-        return inventario;
-    }
-
-    public DataTerceiraEra getDataNascimento() {
-        return this.dataNascimento;
-    }
-
-    public double getNumeroSorte() {
-        double resultado = 101.;
-        boolean ehBissexto = dataNascimento.ehBissexto();
-
-        if (ehBissexto && this.vida >= 80 && this.vida <= 90) {
-            resultado *= -33.0;
-        }
-        //if (!dataNascimento.ehBissexto() && this.nome != null && (this.nome.equals("Seixas") || this.nome.equals("Meireles"))) {
-        if (!ehBissexto && ("Seixas".equals(this.nome) || "Meireles".equals(this.nome))) {
-            resultado = resultado * 33 % 100;
-        }
-
-        return resultado;
-    }
-    
-    public void adicionarItem(Item item) {
-        this.inventario.adicionarItem(item);
-    }
-    
-    public void perderItem(Item item) {
-        this.inventario.removerItem(item);
+        super(nome, dataNascimento);
     }
     
     public void tentarSorte() {
