b2d94262adfc4dcf86c01982ff4de09bc6b09114
fabriciorissetto <fabriciorissetto@gmail.com>
2016-11-01 21:44:52 -0200

    refactor(OverridesValidacao): Movendo overrides de validação para arquivo js

---
+++ src/modulo-05-dot-net/aula-03/StreetFighter.Web/StreetFighter.Web/Scripts/jquery.validate.overrides.js
@@ -0,0 +1,9 @@
+﻿$.validator.methods.number = function (value, element) {
+    var regex = /^(\d*)(\,\d{1,2})?$/; //99999,99
+    return this.optional(element) || regex.test(value);
+}
+
+$.validator.methods.date = function (value, element) {
+    var date = new Date(value.replace(/(\d{2})[/](\d{2})[/](\d{4})/, "$2/$1/$3")); //31/12/2016
+    return this.optional(element) || !/Invalid|NaN/.test(date.toString());
+}
