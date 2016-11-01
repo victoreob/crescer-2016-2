8704e6d02efa755c23842fec9e1bf61de42a0ee7
Fabricio Elias Rissetto da Silva <fabriciosilva@cwi.com.br>
2016-11-01 17:35:10 -0200

    feat(exemplosCadastro): Exemplos de models com data attributes e formulários

---
+++ src/modulo-05-dot-net/aula-04/ExemploModelsComplexas/ExemploModelsComplexas/Controllers/ExemploController.cs
@@ -0,0 +1,50 @@
+﻿using ExemploModelsComplexas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExemploModelsComplexas.Controllers
+{
+    public class ExemploController : Controller
+    {
+        public ActionResult CadastroSimples()
+        {
+            PopularPersonagens();
+
+            return View();
+        }
+        
+        public ActionResult Salvar(ExemploModel model)
+        {
+            PopularPersonagens();
+
+            if (ModelState.IsValid)
+            {
+                ViewBag.Mensagem = "Cadastro concluído com sucesso.";
+                return View("Detalhe", model);
+            }
+            else
+            {
+                ModelState.AddModelError("", "Ocorreu algum erro. Da uma olhada aí pls :(");
+                return View("CadastroSimples");
+            }
+        }
+
+        private void PopularPersonagens()
+        {
+            //ViewBag.ListaPersonagens
+            ViewData["ListaPersonagens"] = new List<SelectListItem>()
+            {
+                new SelectListItem() { Value = "1", Text = "Verde" },
+                new SelectListItem() { Value = "2", Text = "Branco" },
+                new SelectListItem() { Value = "3", Text = "Preto" },
+                new SelectListItem() { Value = "4", Text = "Azul" },
+                new SelectListItem() { Value = "5", Text = "Rosa" },
+                new SelectListItem() { Value = "6", Text = "Amarelo" },
+                new SelectListItem() { Value = "7", Text = "Vermelho" }
+            };
+        }
+    }
+}
---
+++ src/modulo-05-dot-net/aula-04/ExemploModelsComplexas/ExemploModelsComplexas/Models/ExemploModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ExemploModelsComplexas.Models
+{
+    public class ExemploModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        [Range(1, 150, ErrorMessage = "A idade deve ser entre 1 e 150.")]
+        public int? Idade { get; set; }
+
+        [DisplayName("Data de Cadastro")]
+        public DateTime DataCadastro { get; set; }
+
+        public decimal Valor { get; set; }
+
+        [DisplayName("Deseja receber Newsletter?")]
+        public bool QuerReceberNewsletter { get; set; }
+
+        [DisplayName("Gênero")]
+        public char Sexo { get; set; }
+
+        public int IdPowerRangerPreferido { get; set; }
+    }
+}
