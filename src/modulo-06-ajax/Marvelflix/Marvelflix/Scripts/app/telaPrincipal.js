33407040de14aacf6d42968b6bfe20ba4728956d
bernardobrezende <bernardobrezende@gmail.com>
2016-11-10 16:38:10 -0200

    feat(telaPrincipal): listar personagens vindos do servidor.

--- src/modulo-06-ajax/Marvelflix/Marvelflix/Scripts/app/telaPrincipal.js
+++ src/modulo-06-ajax/Marvelflix/Marvelflix/Scripts/app/telaPrincipal.js
@@ -11,14 +11,24 @@ class TelaPrincipal {
 
   renderizarEstadoInicial() {
     $('.tela-centralizada').removeClass('tela-centralizada');
-    this.$elem.show();  
+    this.$elem.show();
+
+    $.get('/api/herois')
+      .done((res) => {
+        marvelflix.render('.tela', 'tela-principal', {
+          chars: res.map(function (item) {
+            return {
+              id: item.id,
+              name: item.nome,
+              thumbnail: item.urlThumbnail
+            }
+          })
+        });
+      });
+    
+    console.log('renderizar com dados estáticos');
    
-    marvelflix.render('.tela', 'tela-principal', {
-      chars: [
-        { id: 1, name: 'Iron Man', thumbnail: 'https://cloud.githubusercontent.com/assets/526075/20180159/652c5846-a740-11e6-8b56-19256fcc52c3.png' },
-        { id: 2, name: 'Spider Man', thumbnail: 'https://cloud.githubusercontent.com/assets/526075/20180159/652c5846-a740-11e6-8b56-19256fcc52c3.png' },
-      ]
-    });
+    
 
   }
 
